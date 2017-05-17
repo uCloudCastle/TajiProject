46ef08873ddd8179cfd9be934e0556924a716d58
randallu <randallu@china-liantong.com>
2017-05-17 18:56:19 +0800

    update

---
+++ app/src/main/java/com/jxlc/tajiproject/algorithm/CheckChangedListener.java
@@ -0,0 +1,8 @@
+package com.jxlc.tajiproject.algorithm;
+
+/**
+ * Created by randal on 2017/5/17.
+ */
+
+public class CheckChangedListener {
+}
---
+++ app/src/main/java/com/jxlc/tajiproject/ui/widgets/StatusIndicatorItem.java
@@ -0,0 +1,10 @@
+package com.jxlc.tajiproject.ui.widgets;
+
+import android.widget.RelativeLayout;
+
+/**
+ * Created by randal on 2017/5/17.
+ */
+
+public class StatusIndicatorItem extends RelativeLayout {
+}
